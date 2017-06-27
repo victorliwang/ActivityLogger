f2f73375ab1ab6738a0b685c5b3637d1d660a7a9
Victor Li Wang <victor@victorliwang.com>
2017-06-27 02:22:28 -0500

    Moved home test code to list directory, created grid with hoursin the first column and spaced out background cards in second column

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,7 +9,8 @@ import moment from 'moment';
 export class HomePage {
 
   now15 = this.nearest15Minutes().format('HH:mm');
-  currentDate = moment().format('MMMM Do, YYYY');
+  currentDate = moment().format('MMM Do');
+  currentDateFull = moment().format('MMMM Do, YYYY');
   currentTime = moment().format('HH:mm');
   in7Hours = this.inXHours(7);
   in9Hours = this.inXHours(8);
@@ -20,6 +21,8 @@ export class HomePage {
     end: this.inXHours(0)
   };
 
+  hours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+
   nextActivity = {
     start: this.lastActivity.end
   }
@@ -29,7 +32,7 @@ export class HomePage {
   }
 
   presentNewActivityModal() {
-    let newActivityModal = this.modalCtrl.create('NewActivityPage');
+    let newActivityModal = this.modalCtrl.create('NewActivityPage', this.nextActivity);
     newActivityModal.onDidDismiss(data => {
       console.log(data);
     });
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ModalController } from 'ionic-angular';
+import moment from 'moment';
 
 @Component({
   selector: 'page-list',
@@ -10,7 +11,7 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
@@ -34,4 +35,43 @@ export class ListPage {
       item: item
     });
   }
+
+  now15 = this.nearest15Minutes().format('HH:mm');
+  currentDate = moment().format('MMM Do');
+  currentDateFull = moment().format('MMMM Do, YYYY');
+  currentTime = moment().format('HH:mm');
+  in7Hours = this.inXHours(7);
+  in9Hours = this.inXHours(8);
+  in15Hours = this.inXHours(15, false);
+  inHalfHour = this.inXHours(.5);
+  lastActivity = {
+    start: this.inXHours(-1),
+    end: this.inXHours(0)
+  };
+
+  hours = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+
+  nextActivity = {
+    start: this.lastActivity.end
+  }
+
+  presentNewActivityModal() {
+    let newActivityModal = this.modalCtrl.create('NewActivityPage', this.nextActivity);
+    newActivityModal.onDidDismiss(data => {
+      console.log(data);
+    });
+    newActivityModal.present();
+  }
+
+  nearest15Minutes(){
+    return moment().add("minutes", 15 - moment().minute() % 15);
+  }
+
+  inXHours(hours: number, fifteen = true) {
+    if(fifteen) {
+      return this.nearest15Minutes().add(hours, 'hour').format('HH:mm');
+    }
+    return moment().add(hours, 'hour').format('HH:mm');
+  }
+
 }
